d59b748cb88dc1568bda679af093a353c8e7bdab
GBAleksandrGB <tomilov.alex@gmail.com>
2022-11-26 12:32:29 +0300

    homework5

---
+++ homework_5/common/utils.py
@@ -0,0 +1,22 @@
+import json
+
+from homework_5.common.decos import log
+from homework_5.common.variables import MAX_PACKAGE_LENGTH, ENCODING
+
+
+@log
+def get_message(client):
+    encoded_response = client.recv(MAX_PACKAGE_LENGTH)
+    json_response = encoded_response.decode(ENCODING)
+    response = json.loads(json_response)
+    if isinstance(response, dict):
+        return response
+    else:
+        raise TypeError
+
+
+@log
+def send_message(sock, message):
+    js_message = json.dumps(message)
+    encoded_message = js_message.encode(ENCODING)
+    sock.send(encoded_message)
